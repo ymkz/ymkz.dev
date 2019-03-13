5464d13ce37a56f2c738950e4a48c5ba71421716
ymkz <ymkzly@gmail.com>
2019-03-13 11:19:25 +0900

    chore: add styling when disabled

--- src/components/elements/submit-button.tsx
+++ src/components/elements/submit-button.tsx
@@ -10,8 +10,10 @@ const SubmitButton = styled.button`
   margin: 1rem 0 0 0;
   padding: 10px 16px;
   :disabled {
-    background-color: ${colors.nord5};
+    /* background-color: ${colors.nord5}; */
     color: ${colors.nord3};
+    cursor: not-allowed;
+    opacity: 0.5;
   }
 `
 
