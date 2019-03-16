284a9672ae63ebeb38ab75c345bf0afc193f5c0e
ymkz <ymkzly@gmail.com>
2019-03-16 15:18:22 +0900

    feat: add optimize react babel plugin and build with esmodules target

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,11 @@ module.exports = function(api) {
   api.cache.using(() => process.env.NODE_ENV)
 
   const presets = [
-    ['next/babel', { 'preset-env': { useBuiltIns: 'usage' } }],
+    ['next/babel', { 'preset-env': { targets: { esmodules: true }, useBuiltIns: 'usage' } }],
     ['@zeit/next-typescript/babel']
   ]
 
-  const plugins = [['emotion']]
+  const plugins = [['emotion'], ['optimize-react']]
 
   return { presets, plugins }
 }
