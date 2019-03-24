f6c8326309185943a2d2ef0b23c30ae18334dd8d
ymkz <ymkzly@gmail.com>
2019-03-25 01:56:45 +0900

    feat: add intersection control for image

--- components/elements/product-image.tsx
+++ components/elements/product-image.tsx
@@ -1,9 +1,33 @@
+import React from 'react'
+import { useIntersection } from 'use-intersection'
 import styled from '@emotion/styled'
 import colors from '../../constants/colors'
 
-const ProductImage = styled.img`
+const ImageContainer = styled.div`
+  height: 256px;
+`
+
+const ImagePlaceholder = styled.div`
+  background-color: ${colors.nord5};
+  border: 1px solid ${colors.nord4};
+  height: 256px;
+  width: 145px;
+`
+
+const Image = styled.img`
   border: 1px solid ${colors.nord4};
   height: 256px;
 `
+
+const ProductImage: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = props => {
+  const target = React.useRef<HTMLDivElement>(null)
+  const intersected = useIntersection(target, { once: true })
+
+  return (
+    <ImageContainer ref={target}>
+      {intersected ? <Image alt="" {...props} /> : <ImagePlaceholder />}
+    </ImageContainer>
+  )
+}
 
 export default ProductImage
--- components/modules/cancelectures.tsx
+++ components/modules/cancelectures.tsx
@@ -40,8 +40,8 @@ function Cancelectures() {
         </ProductLink>
       </ProductSupply>
       <Row space={16}>
-        <ProductImage src="/static/images/cancelectures_native_1.jpg" alt="" height={256} />
-        <ProductImage src="/static/images/cancelectures_native_2.jpg" alt="" height={256} />
+        <ProductImage src="/static/images/cancelectures_native_1.jpg" height={256} />
+        <ProductImage src="/static/images/cancelectures_native_2.jpg" height={256} />
       </Row>
     </ProductContainer>
   )
