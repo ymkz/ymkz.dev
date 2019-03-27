10e4ebeb711910620f88314c3c8e7f19a7377e1d
ymkz <ymkzly@gmail.com>
2019-03-27 14:59:19 +0900

    chore: move to manifest.json in assets

--- components/abstracts/manifest.tsx
+++ components/abstracts/manifest.tsx
@@ -7,7 +7,7 @@ function Manifest() {
       <meta name="theme-color" content={colors.nord6} />
       <link rel="icon" type="image/png" sizes="196x196" href="/static/assets/icon-196x196.png" />
       <link rel="icon" type="image/png" sizes="512x512" href="/static/assets/icon-512x512.png" />
-      <link rel="manifest" href="/static/manifest.json" />
+      <link rel="manifest" href="/static/assets/manifest.json" />
     </React.Fragment>
   )
 }
