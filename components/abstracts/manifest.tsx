897ce35ee0b2853700c79d81d7ec23d66f4332a7
ymkz <ymkzly@gmail.com>
2019-03-27 14:37:29 +0900

    chore: fix assets webicon

--- components/abstracts/manifest.tsx
+++ components/abstracts/manifest.tsx
@@ -5,9 +5,8 @@ function Manifest() {
   return (
     <React.Fragment>
       <meta name="theme-color" content={colors.nord6} />
-      <link rel="icon" type="image/png" sizes="144x144" href="/static/assets/icon_144x144.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/static/assets/icon_192x192.png" />
-      <link rel="icon" type="image/png" sizes="512x512" href="/static/assets/icon_512x512.png" />
+      <link rel="icon" type="image/png" sizes="196x196" href="/static/assets/icon-196x196.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="/static/assets/icon-512x512.png" />
       <link rel="manifest" href="/static/manifest.json" />
     </React.Fragment>
   )
