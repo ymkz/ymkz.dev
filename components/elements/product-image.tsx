30b4cf8c9a00470eed07d08d28eb0d54d19d3dc6
ymkz <ymkzly@gmail.com>
2019-03-29 02:00:23 +0900

    chore: remove intersection handle

--- components/elements/product-image.tsx
+++ components/elements/product-image.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useIntersection } from 'use-intersection'
 import styled from '@emotion/styled'
 import colors from '../../constants/colors'
 
@@ -9,25 +8,15 @@ const ImageContainer = styled.div`
   height: 256px;
 `
 
-const ImagePlaceholder = styled.div`
-  background-color: ${colors.nord5};
-  border: 1px solid ${colors.nord4};
-  height: 256px;
-  width: 145px;
-`
-
 const Image = styled.img`
   border: 1px solid ${colors.nord4};
   height: 256px;
 `
 
 function ProductImage(props: Props) {
-  const target = React.useRef<HTMLDivElement>(null)
-  const intersected = useIntersection(target, { once: true })
-
   return (
-    <ImageContainer ref={target}>
-      {intersected ? <Image alt="" {...props} /> : <ImagePlaceholder />}
+    <ImageContainer>
+      <Image alt="" {...props} />
     </ImageContainer>
   )
 }
