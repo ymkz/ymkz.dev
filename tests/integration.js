968d615080063acb14ef4b1907591a913c6015e9
ymkz <ymkzly@gmail.com>
2019-04-15 16:52:34 +0900

    chore: test res rendering

--- tests/integration.js
+++ tests/integration.js
@@ -4,4 +4,6 @@ const { parse } = require('url')
 
 module.exports = async function(req, res) {
   console.log(req, res, parse(req.url, true))
+  res.setHeader('Content-Type', 'text/html')
+  res.end(`<div>Hello World</div>`)
 }
