31d6175611583ffb68ca1a35ca3d4981755d07c2
ymkz <ymkzly@gmail.com>
2019-04-26 01:49:54 +0900

    feat: add next-offline for pwa with test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const size = require('next-size')
+const offline = require('next-offline')
 const optimize = require('next-optimized-images')
 const typescript = require('@zeit/next-typescript')
 const withPlugins = require('next-compose-plugins')
 
-module.exports = withPlugins([[size], [optimize], [typescript]], {
+module.exports = withPlugins([[size], [offline], [optimize], [typescript]], {
   target: 'serverless',
   webpack: config => config
 })
