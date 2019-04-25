a2b9924aaa46a07de9cf6dbf514f98c2d63d7071
ymkz <ymkzly@gmail.com>
2019-04-26 02:01:50 +0900

    fix: remove next-offline due to fail to register

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const size = require('next-size')
-const offline = require('next-offline')
 const optimize = require('next-optimized-images')
 const typescript = require('@zeit/next-typescript')
 const withPlugins = require('next-compose-plugins')
 
-module.exports = withPlugins([[size], [offline], [optimize], [typescript]], {
+module.exports = withPlugins([[size], [optimize], [typescript]], {
   target: 'serverless',
   webpack: config => config
 })
