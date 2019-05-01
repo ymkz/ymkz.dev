206fbe1ab46eaa55b64f4a34b2efe572750e6475
ymkz <ymkzly@gmail.com>
2019-05-01 23:25:11 +0900

    fix: error build id generate

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const size = require('next-size')
-const buildId = require('next-build-id')
 const optimize = require('next-optimized-images')
 const typescript = require('@zeit/next-typescript')
 const withPlugins = require('next-compose-plugins')
 
 module.exports = withPlugins([[size], [optimize], [typescript]], {
   target: 'serverless',
-  webpack: config => config,
-  generateBuildId: async () => {
-    const fromGit = await buildId({ dir: __dirname })
-    return fromGit.id
-  }
+  webpack: config => config
 })
