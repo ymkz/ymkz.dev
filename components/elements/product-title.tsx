2f09f8d7b95d7b65ceea1a28f96edd4ce3e84bda
ymkz <ymkzly@gmail.com>
2019-05-07 00:02:40 +0900

    fix: line-height if title is multi line

--- components/elements/product-title.tsx
+++ components/elements/product-title.tsx
@@ -5,6 +5,7 @@ const ProductTitle = styled.h1`
   font-family: 'Nunito Sans', sans-serif;
   font-size: 3.6rem;
   font-weight: 900;
+  line-height: 1;
   @media ${breakpoints.mobile} {
     font-size: 2.8rem;
   }
