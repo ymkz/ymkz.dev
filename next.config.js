d2ac5529f1fe5ebb168d143b7b59377b9262b6f1
ymkz <ymkzly@gmail.com>
2019-05-09 21:20:21 +0900

    feat: add service-worker with next-offline

--- next.config.js
+++ next.config.js
@@ -2,11 +2,15 @@
 
 const size = require('next-size')
 const css = require('@zeit/next-css')
+const offline = require('next-offline')
 const optimize = require('next-optimized-images')
 const typescript = require('@zeit/next-typescript')
 const withPlugins = require('next-compose-plugins')
 
-module.exports = withPlugins([[size], [css], [optimize], [typescript]], {
-  target: 'serverless',
-  webpack: config => config
-})
+module.exports = withPlugins(
+  [[size], [css], [optimize], [typescript], [offline, { generateInDevMode: true }]],
+  {
+    target: 'serverless',
+    webpack: config => config
+  }
+)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,20 @@ import '@egoist/snackbar/dist/snackbar.css'
 import React from 'react'
 import Head from 'next/head'
 import App, { Container } from 'next/app'
+// @ts-ignore
+import { register, unregister } from 'next-offline/runtime'
 import GlobalStyle from 'components/abstracts/global-style'
 import Page from 'components/abstracts/page'
 
 export default class extends App {
+  componentDidMount() {
+    register()
+  }
+
+  componentWillUnmount() {
+    unregister()
+  }
+
   render() {
     const { Component, pageProps } = this.props
     return (
