dddb80d46f21a9c35d0eb8c1e59edbebaf6a92aa
ymkz <ymkzly@gmail.com>
2019-05-15 11:57:10 +0900

    feat: add drop-shadow for content readable at scroll

--- components/abstracts/page.tsx
+++ components/abstracts/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import { throttle } from 'throttle-debounce'
 import Header from 'components/modules/header'
 
 type Props = {
@@ -13,10 +14,33 @@ const Main = styled.main`
   padding: 0 16px;
 `
 
+const DropShadow = styled.div`
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 5px 6px -3px inset;
+  height: 6px;
+  left: 0;
+  position: sticky;
+  top: 64px;
+`
+
 function Page({ children }: Props) {
+  const [dropShadowVisible, setDropShadowVisible] = React.useState(false)
+  const handleScroll = throttle(256, () => {
+    if (window.scrollY > 64) {
+      setDropShadowVisible(true)
+    } else {
+      setDropShadowVisible(false)
+    }
+  })
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [handleScroll])
+
   return (
     <React.Fragment>
       <Header />
+      {dropShadowVisible && <DropShadow />}
       <Main>{children}</Main>
     </React.Fragment>
   )
