1e027f0eaed921803a8db4738cbb2465d9014f21
ymkz <ymkzly@gmail.com>
2019-06-04 01:26:41 +0900

    refactor: implement ga with pure react-ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,47 +3,32 @@ import React from 'react'
 import Head from 'next/head'
 import Router from 'next/router'
 import App, { Container } from 'next/app'
-// @ts-ignore
-import withGA from 'next-ga'
-import { createSnackbar } from '@egoist/snackbar'
+import analytics from '~/helpers/analytics'
 import GlobalStyle from '~/components/abstracts/global-style'
 import Page from '~/components/abstracts/page'
 
-export default withGA('UA-140852595-1', Router)(
-  class extends App {
-    componentDidMount() {
-      const ga = localStorage.getItem('ga')
-      if (!ga || ga === 'no') {
-        createSnackbar('Google Analytics is enabled.', {
-          position: 'right',
-          actions: [
-            {
-              text: 'CONFIRM',
-              callback(_, snackbar) {
-                snackbar.destroy()
-                localStorage.setItem('ga', 'yes')
-              }
-            }
-          ]
-        })
-      }
-    }
-
-    render() {
-      const { Component, pageProps } = this.props
-      return (
-        <React.StrictMode>
-          <GlobalStyle />
-          <Container>
-            <Head>
-              <title>ymkz</title>
-            </Head>
-            <Page>
-              <Component {...pageProps} />
-            </Page>
-          </Container>
-        </React.StrictMode>
-      )
-    }
+export default class extends App {
+  componentDidMount() {
+    analytics.requestAuthorization()
+    analytics.initialize()
+    analytics.pageview(location.pathname)
+    Router.onRouteChangeComplete = analytics.pageview
   }
-)
+
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <React.StrictMode>
+        <GlobalStyle />
+        <Container>
+          <Head>
+            <title>ymkz</title>
+          </Head>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </Container>
+      </React.StrictMode>
+    )
+  }
+}
