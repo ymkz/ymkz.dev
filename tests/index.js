1b98dac4e5fda148a739dd5b155848bd58e66eef
ymkz <ymkzly@gmail.com>
2019-06-25 01:09:53 +0900

    chore: modify capture ss function commonality

--- tests/index.js
+++ tests/index.js
@@ -6,9 +6,17 @@
   const { percySnapshot } = require('@percy/puppeteer')
 
   const __CI__ = process.env.CI
-  const url = 'http://localhost:3000'
-
+  const baseUrl = 'http://localhost:3000'
   const screenshotsDir = await makeDir('tests/screenshots')
+
+  // @ts-ignore
+  async function screenshot(page, name) {
+    if (__CI__) {
+      await percySnapshot(page, name)
+    } else {
+      await page.screenshot({ fullPage: true, path: `${screenshotsDir}/${name}.png` })
+    }
+  }
 
   // Setup browser
   const browser = await puppeteer.launch({
@@ -19,28 +27,16 @@
   await page.setViewport({ width: 1920, height: 1080 })
 
   // Test index page
-  await page.goto(url)
-  if (__CI__) {
-    await percySnapshot(page, '/')
-  } else {
-    await page.screenshot({ fullPage: true, path: `${screenshotsDir}/index.png` })
-  }
+  await page.goto(baseUrl)
+  await screenshot(page, 'index')
 
   // Test /about page
-  await page.goto(`${url}/about`)
-  if (__CI__) {
-    await percySnapshot(page, '/about')
-  } else {
-    await page.screenshot({ fullPage: true, path: `${screenshotsDir}/about.png` })
-  }
+  await page.goto(`${baseUrl}/about`)
+  await screenshot(page, 'about')
 
   // Test /work page
-  await page.goto(`${url}/work`)
-  if (__CI__) {
-    await percySnapshot(page, '/work')
-  } else {
-    await page.screenshot({ fullPage: true, path: `${screenshotsDir}/work.png` })
-  }
+  await page.goto(`${baseUrl}/work`)
+  await screenshot(page, 'work')
 
   browser.close()
 })()
