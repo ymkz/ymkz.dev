cca8ed3d95f90f910a3b532cb0ccc6554a8af5ab
ymkz <ymkzly@gmail.com>
2019-08-18 17:19:55 +0900

    chore(theme): split theme object

--- helpers/snackbar.ts
+++ helpers/snackbar.ts
@@ -2,20 +2,19 @@ import { createSnackbar } from '@egoist/snackbar'
 import { getTheme } from '~/helpers/theming'
 
 export function snackbar(message: string, error?: boolean) {
+  const lightTheme = {
+    backgroundColor: '#2e3440',
+    actionColor: '#4c566a',
+    textColor: '#eceff4'
+  }
+  const darkTheme = {
+    backgroundColor: '#eceff4',
+    actionColor: '#4c566a',
+    textColor: '#2e3440'
+  }
   return createSnackbar(message, {
-    timeout: error ? undefined : 4000,
     position: 'right',
-    theme:
-      getTheme() === 'light'
-        ? {
-            backgroundColor: '#2e3440',
-            actionColor: '#4c566a',
-            textColor: '#eceff4'
-          }
-        : {
-            backgroundColor: '#eceff4',
-            actionColor: '#4c566a',
-            textColor: '#2e3440'
-          }
+    timeout: error ? undefined : 4000,
+    theme: getTheme() === 'light' ? lightTheme : darkTheme
   })
 }
