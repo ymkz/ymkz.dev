f05b9e07b41eb35a165a14c5795629307bc9d614
ymkz <ymkzly@gmail.com>
2019-08-18 17:16:00 +0900

    chore(e2e): use percy if PERCY_TOKEN is set

--- e2e/index.js
+++ e2e/index.js
@@ -1,25 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 !(async () => {
+  const path = require('path')
   const makeDir = require('make-dir')
   const puppeteer = require('puppeteer')
   const { percySnapshot } = require('@percy/puppeteer')
 
-  const __CI__ = process.env.CI
   const baseUrl = 'http://localhost:3000'
   const screenshotsDir = await makeDir('e2e/__screenshots__')
 
   async function screenshot(page, name) {
-    if (__CI__) {
+    if (process.env.PERCY_TOKEN) {
       await percySnapshot(page, name)
     } else {
-      await page.screenshot({ fullPage: true, path: `${screenshotsDir}/${name}.png` })
+      await page.screenshot({ fullPage: true, path: path.join(screenshotsDir, `${name}.png`) })
     }
   }
 
   const option = { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] }
   const browser = await puppeteer.launch(option)
   const page = await browser.newPage()
+
   await page.setViewport({ width: 1920, height: 1080 })
 
   await page.goto(baseUrl)
