4dc5ee08e8f0050af8762cb1cd22af363ad942bb
ymkz <ymkzly@gmail.com>
2019-09-22 21:53:25 +0900

    chore(babel): remove useless settings

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,8 @@
 module.exports = api => {
   api.cache(true)
-
   return {
     presets: [
-      [
-        'next/babel',
-        { 'preset-env': { corejs: 3, targets: { esmodules: true }, useBuiltIns: 'usage' } }
-      ],
+      ['next/babel'],
       ['@emotion/babel-preset-css-prop', { labelFormat: '[dirname]__[filename]__[local]' }]
     ],
     plugins: [['module-resolver', { alias: { '~': '.' }, extensions: ['.js', '.ts', '.tsx'] }]]
