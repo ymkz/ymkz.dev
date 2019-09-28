2172fe26f6d9d7d33bbd4c65a67d0036a8e45738
ymkz <ymkzly@gmail.com>
2019-09-28 14:32:14 +0900

    refactor(e2e): optimize to load module with env

--- e2e/index.js
+++ e2e/index.js
@@ -1,28 +1,27 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 !(async () => {
-  const path = require('path')
-  const makeDir = require('make-dir')
   const puppeteer = require('puppeteer')
-  const { percySnapshot } = require('@percy/puppeteer')
 
-  const baseUrl = 'http://localhost:3000'
-  const timeoutOption = { timeout: 10000 }
-  const waitOption = { waitUntil: 'domcontentloaded' }
-  const percyOptions = { widths: [415, 1920], minHeight: 1080 }
-
-  const screenshotsDir = await makeDir('e2e/__screenshots__')
+  const isLocal = !process.env.PERCY_TOKEN
+  const path = isLocal && require('path')
+  const makeDir = isLocal && require('make-dir')
+  const screenshotsDir = isLocal && (await makeDir(`e2e/__screenshots__/${Date.now()}`))
+  const percySnapshot = !isLocal && require('@percy/puppeteer').percySnapshot
 
   async function screenshot(page, name) {
     if (process.env.PERCY_TOKEN) {
-      await percySnapshot(page, name, percyOptions)
+      await percySnapshot(page, name, { widths: [415, 1920], minHeight: 1080 })
     } else {
       await page.screenshot({ fullPage: true, path: path.join(screenshotsDir, `${name}.png`) })
     }
   }
 
-  const option = { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] }
-  const browser = await puppeteer.launch(option)
+  const baseUrl = 'http://localhost:3000'
+  const timeoutOption = { timeout: 10000 }
+  const waitOption = { waitUntil: 'domcontentloaded' }
+  const puppeteerOption = { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+  const browser = await puppeteer.launch(puppeteerOption)
   const page = await browser.newPage()
 
   await page.setViewport({ width: 1920, height: 1080 })
