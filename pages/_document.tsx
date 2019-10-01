b591a604cc66652c602769519104ab73f4a1469f
ymkz <ymkzly@gmail.com>
2019-10-02 04:56:03 +0900

    perf(font): add crossorigin attrs for webhint optimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,9 +7,9 @@ class Document extends NextDocument {
       <html lang="ja">
         <Head>
           <meta name="theme-color" content="#eceff4" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="description" content="ymkz's portfolio website" />
           <link rel="manifest" href="/static/assets/manifest.json" />
+          <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="anonymous" />
           <link rel="icon" type="image/png" sizes="96x96" href="/static/assets/icon-96x96.png" />
           <link
             rel="icon"
@@ -31,6 +31,7 @@ class Document extends NextDocument {
           />
           <link
             rel="stylesheet"
+            crossOrigin="anonymous"
             href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,900&font-display=swap"
           />
         </Head>
