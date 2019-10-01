b1714a810348687616614fd15c856f7e54529d56
ymkz <ymkzly@gmail.com>
2019-10-01 15:48:20 +0900

    chore(module): app-bar styled on outside

--- components/modules/app-bar.tsx
+++ components/modules/app-bar.tsx
@@ -1,4 +1,4 @@
-import { css } from '@emotion/core'
+import styled from '@emotion/styled'
 import React from 'react'
 
 import { FlexRow } from '~/components/elements/flex-row'
@@ -8,25 +8,7 @@ import { up } from '~/helpers/breakpoints'
 
 export const AppBar = () => {
   return (
-    <header
-      css={css`
-        position: sticky;
-        top: 0;
-        left: 0;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        width: 100%;
-        height: 52px;
-        padding: 0 16px;
-        background-color: var(--base-background);
-        ${up('mobile')} {
-          max-width: 768px;
-          height: 64px;
-          margin: 0 auto;
-        }
-      `}
-    >
+    <Container>
       <FlexRow space={8}>
         <NavIcon />
         <NavLink href="/">ymkz</NavLink>
@@ -35,6 +17,24 @@ export const AppBar = () => {
         <NavLink href="/about">about</NavLink>
         <NavLink href="/work">work</NavLink>
       </FlexRow>
-    </header>
+    </Container>
   )
 }
+
+const Container = styled.header`
+  position: sticky;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 52px;
+  padding: 0 16px;
+  background-color: var(--base-background);
+  ${up('mobile')} {
+    max-width: 768px;
+    height: 64px;
+    margin: 0 auto;
+  }
+`
