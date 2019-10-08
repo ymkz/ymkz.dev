f578c79af2b64a61bdc1777ec30603c7a8162d51
ymkz <ymkzly@gmail.com>
2019-10-08 16:49:21 +0900

    chore: move some directory for clean-up

--- src/components/elements/nav-icon.tsx
+++ src/components/elements/nav-icon.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { switchTheme } from '~/helpers/theming'
-import icon from '~~/templates/icon.svg?inline'
+import icon from '~/images/icons/icon.svg?inline'
 
 export const NavIcon = () => {
   return <img src={icon} alt="" width={24} height={24} onClick={switchTheme} />
