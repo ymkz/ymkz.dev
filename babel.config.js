771b53cdd0ca710e45af192dc892210f380ba2b4
ymkz <ymkzly@gmail.com>
2019-12-27 13:46:47 +0900

    chore(babel): update babel config with emotion

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
 module.exports = api => {
   api.cache(true)
   return {
-    presets: [
-      ['next/babel'],
-      ['@emotion/babel-preset-css-prop', { labelFormat: '-[dirname]__[filename]__[local]' }],
-    ],
+    presets: [['next/babel'], ['@emotion/babel-preset-css-prop']],
     plugins: [['module-resolver', { alias: { '~': './src' }, extensions: ['.js', '.ts', '.tsx'] }]],
   }
 }
