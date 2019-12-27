e1a0a5147c627bc69db3a75b1548c11a3e9318fb
ymkz <ymkzly@gmail.com>
2019-12-27 13:42:53 +0900

    chore(deps): remove framer-motion

--- src/components/templates/page-container.tsx
+++ src/components/templates/page-container.tsx
@@ -1,26 +1,13 @@
 import styled from '@emotion/styled'
-import { AnimatePresence, motion, Variants } from 'framer-motion'
 import React from 'react'
 
 import { up } from '~/helpers/breakpoints'
 
-const variants: Variants = {
-  initial: { opacity: 0 },
-  in: { opacity: 1 },
-  out: { opacity: 0 },
-}
-
 export const PageContainer: React.FC = ({ children }) => {
-  return (
-    <AnimatePresence exitBeforeEnter>
-      <Container initial="initial" animate="in" exit="out" variants={variants}>
-        {children}
-      </Container>
-    </AnimatePresence>
-  )
+  return <Container>{children}</Container>
 }
 
-const Container = styled(motion.main)`
+const Container = styled.main`
   margin: 0 auto;
   max-width: 672px;
   padding: 32px 16px;
