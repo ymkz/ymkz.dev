a7282f12beca2dfd30dd56143e955f73154c5864
ymkz <ymkzly@gmail.com>
2020-01-16 23:23:24 +0900

    refactor(config): remove unnecessary nextjs config plugins

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const compose = require('next-compose-plugins')
-const css = require('@zeit/next-css')
-
-const nextOption = {
+module.exports = {
   poweredByHeader: false,
   reactStrictMode: true,
   devIndicators: { autoPrerender: false },
 }
-
-module.exports = compose([[css]], nextOption)
