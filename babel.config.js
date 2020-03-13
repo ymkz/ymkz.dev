c2ee44578a2096add5ebc8314687c0228c3c7db5
ymkz <ymkzly@gmail.com>
2020-03-14 02:27:39 +0900

    chore: remove unused babel config and add alias to preact config

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = api => {
-  api.cache(true)
-  return {
-    presets: [['preact-cli/babel']],
-    plugins: [['module-resolver', { alias: { '~': './src' }, extensions: ['.ts', '.tsx'] }]],
-  }
-}
--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,7 @@
 import copy from 'copy-webpack-plugin'
+import path from 'path'
 
 export default config => {
+  config.resolve.alias['~'] = path.resolve(__dirname, 'src')
   config.plugins.push(new copy([{ context: `${__dirname}/public`, from: `*.*` }]))
 }
