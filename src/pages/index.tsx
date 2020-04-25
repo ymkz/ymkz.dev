669a64be07c2519b25c0657a92be769f17fd531f
ymkz <ymkzly@gmail.com>
2020-04-25 13:10:14 +0900

    feat: update more simplify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,101 +6,100 @@ import { LinkedIn } from '../components/icon-linkedin'
 import { Twitter } from '../components/icon-twitter'
 import { Wind } from '../components/icon-wind'
 import { LinkExtern } from '../components/link-extern'
-import style from '../styles/index.module.scss'
 
 const Index = () => {
   return (
     <React.Fragment>
-      <h1 className={style.motto}>
+      <h1 className="motto">
         Catch
         <br />
         the
         <br />
         wind.
       </h1>
-      <div className={style.totem}>
+      <div className="totem">
         <span>[&nbsp;</span>
         <Wind />
         <span>&nbsp;]</span>
       </div>
-      <p className={style.paragraph}>
+      <p className="paragraph">
         I&apos;m Kazuya Yamashita. a.k.a ymkz.
         <br />
         24y/o, based in Tokyo, Japan.
         <br />
         Working as a frontend engineer.
       </p>
-      <ul className={style.listRow}>
-        <li className={style.listRowItem}>React</li>
-        <li className={style.listRowItem}>TypeScript</li>
-        <li className={style.listRowItem}>Node.js</li>
-        <li className={style.listRowItem}>Next.js</li>
-        <li className={style.listRowItem}>React Native</li>
-        <li className={style.listRowItem}>Redux</li>
-        <li className={style.listRowItem}>Webpack</li>
-        <li className={style.listRowItem}>HTML5</li>
-        <li className={style.listRowItem}>CSS3</li>
+      <ul className="list-row">
+        <li className="list-row__item">React</li>
+        <li className="list-row__item">TypeScript</li>
+        <li className="list-row__item">Node.js</li>
+        <li className="list-row__item">Next.js</li>
+        <li className="list-row__item">React Native</li>
+        <li className="list-row__item">Redux</li>
+        <li className="list-row__item">Webpack</li>
+        <li className="list-row__item">HTML5</li>
+        <li className="list-row__item">CSS3</li>
       </ul>
-      <p className={style.paragraph}>
+      <p className="paragraph">
         Majored in computer science, studied human-computer interaction with face/eye tracking.
         <br />
         Master of Engineering.
       </p>
-      <ul className={style.listColumn}>
-        <li className={style.listColumnItem}>Shimane University</li>
-        <li className={style.listColumnItem}>Bachelor: 2014.4 - 2018.3</li>
-        <li className={style.listColumnItem}>Master: 2018.4 - 2020.3</li>
+      <ul className="list-column">
+        <li className="list-column__item">Shimane University</li>
+        <li className="list-column__item">Bachelor: 2014.4 - 2018.3</li>
+        <li className="list-column__item">Master: 2018.4 - 2020.3</li>
       </ul>
-      <p className={style.paragraph}>
+      <p className="paragraph">
         Social account is here, get in touch with me.
         <br />
         Twitter DM is open.
       </p>
-      <ul className={style.listRow}>
-        <li className={style.listRowBadge}>
-          <div className={style.listRowBadgeIcon}>
+      <ul className="list-row">
+        <li className="list-row__badge">
+          <div className="list-row__badge--icon">
             <Twitter />
           </div>
-          <LinkExtern className={style.anchor} href="https://twitter.com/ymkzly">
+          <LinkExtern className="anchor" href="https://twitter.com/ymkzly">
             Twitter
           </LinkExtern>
         </li>
-        <li className={style.listRowBadge}>
-          <div className={style.listRowBadgeIcon}>
+        <li className="list-row__badge">
+          <div className="list-row__badge--icon">
             <Facebook />
           </div>
-          <LinkExtern className={style.anchor} href="https://facebook.com/ymkzly">
+          <LinkExtern className="anchor" href="https://facebook.com/ymkzly">
             Facebook
           </LinkExtern>
         </li>
-        <li className={style.listRowBadge}>
-          <div className={style.listRowBadgeIcon}>
+        <li className="list-row__badge">
+          <div className="list-row__badge--icon">
             <GitHub />
           </div>
-          <LinkExtern className={style.anchor} href="https://github.com/ymkz">
+          <LinkExtern className="anchor" href="https://github.com/ymkz">
             GitHub
           </LinkExtern>
         </li>
-        <li className={style.listRowBadge}>
-          <div className={style.listRowBadgeIcon}>
+        <li className="list-row__badge">
+          <div className="list-row__badge--icon">
             <LinkedIn />
           </div>
-          <LinkExtern className={style.anchor} href="https://linkedin.com/in/ymkz">
+          <LinkExtern className="anchor" href="https://linkedin.com/in/ymkz">
             LinkedIn
           </LinkExtern>
         </li>
       </ul>
-      <p className={style.paragraph}>
+      <p className="paragraph">
         I Like Padding, Sci-Fi, FPS Games.
         <br />
         Play games seriously with fun.
       </p>
-      <ul className={style.listColumn}>
-        <li className={style.listColumnItem}>ARIA(天野こずえ)</li>
-        <li className={style.listColumnItem}>ハーモニー(伊藤計劃)</li>
-        <li className={style.listColumnItem}>CS:GO</li>
-        <li className={style.listColumnItem}>Overwatch</li>
-        <li className={style.listColumnItem}>PUBG</li>
+      <ul className="list-column">
+        <li className="list-column__item">ARIA(天野こずえ)</li>
+        <li className="list-column__item">ハーモニー(伊藤計劃)</li>
+        <li className="list-column__item">CS:GO</li>
+        <li className="list-column__item">Overwatch</li>
+        <li className="list-column__item">PUBG</li>
       </ul>
     </React.Fragment>
   )
