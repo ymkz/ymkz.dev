7ecde55885e7a46cbe13eb8be9a08ff749999d7a
ymkz <ymkzly@gmail.com>
2020-06-27 19:46:36 +0900

    feat: add goober to css-in-js styling

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,17 @@
 import { NextPage } from 'next'
 import React from 'react'
+import { styled } from 'goober'
+
+const StatusCode = styled('div')`
+  font-weight: 800;
+  font-size: 128px;
+  font-family: 'Montserrat', sans-serif;
+`
 
 const NotFound: NextPage = () => {
   return (
     <React.Fragment>
-      <div className="not-found__status-code">404</div>
-      <style jsx>{`
-        .not-found__status-code {
-          font-weight: 800;
-          font-size: 128px;
-          font-family: 'Montserrat', sans-serif;
-        }
-      `}</style>
+      <StatusCode>404</StatusCode>
     </React.Fragment>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../styles/reset.css'
 import '../styles/style.css'
 
+import { setPragma } from 'goober'
 import { DefaultSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 import React from 'react'
+
+setPragma(React.createElement)
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
