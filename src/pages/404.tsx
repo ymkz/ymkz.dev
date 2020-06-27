efee8c6da63ec4b39e20308f80a6ea2aa3d665fe
ymkz <ymkzly@gmail.com>
2020-06-27 19:33:35 +0900

    chore: move styled-jsx 404 page styling

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,8 +4,14 @@ import React from 'react'
 const NotFound: NextPage = () => {
   return (
     <React.Fragment>
-      <div className="not-found__title">404</div>
-      <div className="not-found__subtitle">PAGE NOT FOUND</div>
+      <div className="not-found__status-code">404</div>
+      <style jsx>{`
+        .not-found__status-code {
+          font-weight: 800;
+          font-size: 128px;
+          font-family: 'Montserrat', sans-serif;
+        }
+      `}</style>
     </React.Fragment>
   )
 }
