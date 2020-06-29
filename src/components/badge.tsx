f541e917dd52056afd12c944f023bef3c57f7ad9
ymkz <ymkzly@gmail.com>
2020-06-29 22:53:25 +0900

    chore: modify gap of list row item

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Badge = ({ name, href, icon }: Props) => {
   return (
-    <li className="margin-botom-8 border border-left-0">
+    <li className="margin-bottom-8 border border-left-0">
       <a className="color-black link flex-center-vertical" target="_blank" rel="noopener noreferrer" href={href}>
         <div className="color-white bg-color-black size-24 flex-center">{icon}</div>
         <div className="padding-horizontal-8 montserrat">{name}</div>
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -11,15 +11,15 @@ export const Profile = () => {
         Working as a frontend engineer.
       </p>
       <ul className="flex gap-8">
-        <li className="padding-vertical-4 padding-horizontal-8 montserrat border">React</li>
-        <li className="padding-vertical-4 padding-horizontal-8 montserrat border">TypeScript</li>
-        <li className="padding-vertical-4 padding-horizontal-8 montserrat border">Node.js</li>
-        <li className="padding-vertical-4 padding-horizontal-8 montserrat border">Next.js</li>
-        <li className="padding-vertical-4 padding-horizontal-8 montserrat border">React Native</li>
-        <li className="padding-vertical-4 padding-horizontal-8 montserrat border">Redux</li>
-        <li className="padding-vertical-4 padding-horizontal-8 montserrat border">Webpack</li>
-        <li className="padding-vertical-4 padding-horizontal-8 montserrat border">HTML5</li>
-        <li className="padding-vertical-4 padding-horizontal-8 montserrat border">CSS3</li>
+        <li className="padding-vertical-4 padding-horizontal-8 margin-bottom-8 montserrat border">React</li>
+        <li className="padding-vertical-4 padding-horizontal-8 margin-bottom-8 montserrat border">TypeScript</li>
+        <li className="padding-vertical-4 padding-horizontal-8 margin-bottom-8 montserrat border">Node.js</li>
+        <li className="padding-vertical-4 padding-horizontal-8 margin-bottom-8 montserrat border">Next.js</li>
+        <li className="padding-vertical-4 padding-horizontal-8 margin-bottom-8 montserrat border">React Native</li>
+        <li className="padding-vertical-4 padding-horizontal-8 margin-bottom-8 montserrat border">Redux</li>
+        <li className="padding-vertical-4 padding-horizontal-8 margin-bottom-8 montserrat border">Webpack</li>
+        <li className="padding-vertical-4 padding-horizontal-8 margin-bottom-8 montserrat border">HTML5</li>
+        <li className="padding-vertical-4 padding-horizontal-8 margin-bottom-8 montserrat border">CSS3</li>
       </ul>
     </section>
   )
