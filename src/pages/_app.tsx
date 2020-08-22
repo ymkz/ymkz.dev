4712e2dc27dafe366be058f4d9670b200554791e
ymkz <ymkzly@gmail.com>
2020-08-22 19:34:56 +0900

    chore: remove dynamic theming

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,16 @@ import '../styles/reset.css'
 import '../styles/style.css'
 
 import { DefaultSeo } from 'next-seo'
-import withDarkMode, { useDarkMode } from 'next-dark-mode'
 import { AppProps } from 'next/app'
 import React from 'react'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const { darkModeActive } = useDarkMode()
   return (
     <React.Fragment>
       <DefaultSeo title="ymkz" />
-      <div className={`root ${darkModeActive ? 'dark' : 'light'}`}>
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </React.Fragment>
   )
 }
 
-export default withDarkMode(App)
+export default App
