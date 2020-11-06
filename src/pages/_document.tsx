85dfd8278f4d1fbe075dd196fbde810654a9e582
ymkz <ymkzly@gmail.com>
2020-11-06 23:04:33 +0900

    feat: add public resources

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,12 @@ class Document extends NextDocument {
       <Html lang="ja">
         <Head>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&display=swap" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <noscript>You need to enable JavaScript to run this app.</noscript>
