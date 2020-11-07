a7b9e82777cdf6a87b02399c8b1eadcd19929173
ymkz <ymkzly@gmail.com>
2020-11-07 14:00:47 +0900

    chore: reduce font size by text parameter

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,10 @@ class Document extends NextDocument {
     return (
       <Html lang="ja">
         <Head>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&display=swap" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&text=Catchthewind.&display=swap"
+          />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
