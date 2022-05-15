0e152907a92b6461bf1ffef0b0fcd2580361ca7b
ymkz <ymkz@users.noreply.github.com>
2022-05-15 21:42:33 +0900

    feat: pure html dist

--- src/routes/robots.txt.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from 'hono'
-import txt from '~/public/robots.txt'
-
-export const robots = (ctx: Context<never>) => {
-  ctx.header('content-type', 'text/plain')
-  return ctx.body(txt)
-}
